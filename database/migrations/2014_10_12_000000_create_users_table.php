e687112e4de7e30ea3a7ecd793f41c98e15e7403
Bagus03 <mbagus09prl@gmail.com>
2025-05-31 21:36:09 +0800

    Add files via upload

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,33 @@
+@extends('layouts.main')
+@section('content')
+<div class="container mt-4">
+    <div class="text-center">
+        <h3>Register page</h3>
+        @foreach($erorrs->all() as $error)
+        <div class="alert alert-danger">
+            {{$error}}
+        </div>
+        @endforeach
+    </div>
+    <form action"{{route('register.post')}}" method="post"
+</div>
+
+return new class extends Migration
+{
+
+
+
+
+
+
+
+
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    
